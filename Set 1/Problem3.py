c7504809f652197eb971391aa69319826e25c2a0
EVAN-SOOBIN-JEON <ejeon2@uci.edu>
2025-04-09 16:29:55 -0700

    problem3.py

--- Set 1/Problem3.py
+++ Set 1/Problem3.py
@@ -1,12 +1,25 @@
 import unittest
 from printing import print_values_in_range, print_reversed_list
+import io
+import sys
 
-class Test(unittest.TestCase):
+
+class TestPrintingFunctions(unittest.TestCase):
     def test_print_values_in_range(self):
-        self.assertEqual(print_values_in_range(1, 6, 2), [1, 3, 5])
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        print_values_in_range(1, 6, 2)
+        sys.stdout = sys.__stdout__
+        expected_output = "1\n3\n5\n"
+        self.assertEqual(captured_output.getvalue(), expected_output)
 
     def test_print_reversed_list(self):
-        self.assertEqual(print_reversed_list('1234'), ['4321'])
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        print_reversed_list(["1234"])
+        sys.stdout = sys.__stdout__
+        expected_output = "4321\n"
+        self.assertEqual(captured_output.getvalue(), expected_output)
 
 
 if __name__ == '__main__':
