1d9b608930a8e339468ba2e415db08023361ce54
EVAN-SOOBIN-JEON <ejeon2@uci.edu>
2025-04-09 16:39:01 -0700

    problem3.py

--- Set 1/Problem3.py
+++ Set 1/Problem3.py
@@ -13,12 +13,20 @@ class TestPrintingFunctions(unittest.TestCase):
         expected_output = "1\n3\n5\n"
         self.assertEqual(captured_output.getvalue(), expected_output)
 
-    def test_print_reversed_list(self):
+    def test_print_reversed_list_single(self):
         captured_output = io.StringIO()
         sys.stdout = captured_output
         print_reversed_list(["1234"])
         sys.stdout = sys.__stdout__
         expected_output = "4321\n"
+        self.assertEqual(captured_output.getvalue(), expected_output)
+
+    def test_print_reversed_list_multiple(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        print_reversed_list(["1234", "4321"])
+        sys.stdout = sys.__stdout__
+        expected_output = "4321\n1234\n"
         self.assertEqual(captured_output.getvalue(), expected_output)
 
 
--- Set 1/printing.py
+++ Set 1/printing.py
@@ -20,5 +20,5 @@ def print_reversed_list(values):
         print(value[::-1])
 
 
-print(print_values_in_range(1, 6, 2))
+print(print_values_in_range('a', 6, 2))
 print(print_reversed_list(['1234', '678']))
