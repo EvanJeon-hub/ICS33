215ef8af7487e8fc53a290019c353b8c1ea3e4a7
EVAN-SOOBIN JEON <ejeon2@uci.edu>
2025-04-10 21:06:46 -0700

    finished

--- Set 1/Problem4.py
+++ Set 1/Problem4.py
@@ -2,11 +2,25 @@
 # Evan-Soobin Jeon
 # 3537131
 # ejeon2@uci.edu
+
+
 class CustomTypeError(Exception):
     pass
 
+
 class ExampleContextManager(Exception):
-    pass
+    def __init__(self, expected_exception):
+        self.expected_exception = expected_exception
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type is issubclass(exc_type, self.expected_exception):
+            return True
+        raise CustomTypeError(f"{self.expected_exception} is not raised"
+                              f"but raised {exc_type if exc_type else 'no exception'}")
+
 
 def should_raise(sample_exception):
     return ExampleContextManager(sample_exception)
