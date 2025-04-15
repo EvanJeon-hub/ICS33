489375344c7d4b8afcf09ab8f33ed51ab1a264ce
EVAN-SOOBIN JEON <ejeon2@uci.edu>
2025-04-15 01:14:09 -0700

    added some explaination

--- devices.py
+++ devices.py
@@ -31,18 +31,23 @@ class Device:
             raise TypeError("target_device must be a Device instance.")
         if target_device.device_id < 0 or delay < 0:
             raise ValueError("must be non-negative integers.")
+        # Adds the device and delay to the propagation_set dictionary
         self.propagation_set[target_device] = delay
 
     def receive_alert(self, alert: Alert, current_time: int, queue: list):
         """""Receive an alert and propagate it to other devices."""
+        # Check if the alert is already recorded
         if alert.description in self.notified_alerts:
             return
 
+        # record the alert status
         self.notified_alerts.add(alert.description)
+        # print the alert message
         print(alert.create_receive_alert_message
               (self.device_id, alert.device_id, current_time)
               )
 
+        # Propagate the alert to other devices
         for target_device, delay in self.propagation_set.items():
             propagation_time = current_time + delay
             new_alert = Alert(
@@ -56,14 +61,18 @@ class Device:
     def receive_cancellation(
             self, cancel: Cancellation, current_time: int, queue: list):
         """Receive a cancellation and propagate it to other devices."""
+        # Check if the cancellation is already recorded
         if cancel.description in self.canceled_alerts:
             return
 
+        # record the cancellation status
         self.canceled_alerts.add(cancel.description)
+        # print the cancellation message
         print(cancel.create_receive_cancel_message
               (self.device_id, cancel.device_id, current_time)
               )
 
+        # Propagate the cancellation to other devices
         for target_device, delay in self.propagation_set.items():
             cancellation_time = current_time + delay
             new_cancellation = Cancellation(
@@ -80,9 +89,11 @@ class Device:
     def raise_alert(self, description, time, queue):
         """Raise an alert and propagate it to other devices."""
         alert = Alert(self.device_id, description, time)
+        # scheduling propagation to other devices
         self.receive_alert(alert, time, queue)
 
     def cancel_alert(self, description, time, queue):
         """Cancel an alert and propagate it to other devices."""
         cancellation = Cancellation(self.device_id, description, time)
+        # scheduling propagation to other devices
         self.receive_cancellation(cancellation, time, queue)
