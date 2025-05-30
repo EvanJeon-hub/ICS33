892a5c11bd808df29cd4c0bf4d41d6df8c5a51f3
EVAN-SOOBIN JEON <ejeon2@uci.edu>
2025-05-29 22:37:40 -0700

    implementing parser

--- grammar_parser.py
+++ grammar_parser.py
@@ -2,16 +2,23 @@
 # ejeon2@uci.edu
 import random
 from grammar_symbol import parse_symbol
+"""
+Rule: HowIsBoo
+{
+HowIsBoo - Start Variable
+1 Boo is [Adjective] today - Option
+}
 
-class Option:
-    def __init__(self, chance, symbols):
-        self.chance = chance
-        self.symbols = [parse_symbol(s) for s in symbols]
-
-    def generate(self, grammar):
-        for symbol in self.symbols:
-            yield from symbol.find(grammar)
-
+Rule: Adjective
+{
+Adjective - Variable
+3 happy    - Options
+3 perfect
+1 relaxing
+1 fulfilled
+2 excited
+}
+"""
 
 class Rule:
     def __init__(self, variable, options):
@@ -24,6 +31,16 @@ class Rule:
         return selected_option.generate(grammar)
 
 
+class Option:
+    def __init__(self, chance, symbols):
+        self.chance = chance
+        self.symbols = [parse_symbol(s) for s in symbols]
+
+    def generate(self, grammar):
+        for symbol in self.symbols:
+            yield from symbol.find(grammar)
+
+
 class Grammar:
     def __init__(self):
         self._rules = {}
@@ -33,4 +50,28 @@ class Grammar:
 
     def get_rule(self, variable):
         return self._rules[variable]
+
+    @staticmethod
+    def generate_file(path):
+        grammar = Grammar()
+        with open(path, encoding = 'utf-8') as file:
+            lines = [line.strip() for line in file if line.strip()]
+
+            i = 0
+            while i < len(lines):
+                if lines[i] == "{":
+                    i += 1
+                    variable = lines[i]
+                    i += 1
+                    options = []
+                    while lines[i] != "}":
+                        parts = lines[i].split()
+                        chance = int(parts[0])
+                        symbols = parts[1:]
+                        options.append(Option(chance, symbols))
+                        i += 1
+                    grammar.add_rule(Rule(variable, options))
+                i += 1
+        return grammar
+
 
--- project4.py
+++ project4.py
@@ -2,15 +2,10 @@
 #
 # ICS 33 Spring 2025
 # Project 4: Still Looking for Something
-from grammar_parser import Grammar
+from grammar_parser import Grammar, Rule, Option
 from grammar_symbol import VariableSymbol
 
-def main():
-    grammar_file = input().strip()
-    count = int(input().strip())
-    start = input().strip()
 
-    pass
 
 
 if __name__ == '__main__':
