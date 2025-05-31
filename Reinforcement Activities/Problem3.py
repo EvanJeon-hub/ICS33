fdbfc3e057c508cee47d606730d630b25bd85477
EVAN-SOOBIN JEON <ejeon2@uci.edu>
2025-05-30 20:45:57 -0700

    reorganize

--- Reinforcement Activities/Problem3.py
+++ Reinforcement Activities/Problem3.py
@@ -1,2 +1,27 @@
 # Evan-Soobin Jeon
 # ejeon2@uci.edu
+import random
+
+def cached(size):
+    def decorator(func):
+        cache = {}
+
+        def helper(*args, **kwargs):
+            try:
+                key = (args, frozenset(kwargs.items()))
+            except TypeError:
+                return func(*args, **kwargs)
+
+            if key in cache:
+                return cache[key]
+
+            result = func(*args, **kwargs)
+
+            if len(cache) >= size:
+                random_key = random.choice(list(cache.keys()))
+                del cache[random_key]
+
+            cache[key] = result
+            return result
+        return helper
+    return decorator
--- Reinforcement Activities/Problem4.py
+++ Reinforcement Activities/Problem4.py
@@ -1,2 +1,25 @@
 # Evan-Soobin Jeon
 # ejeon2@uci.edu
+import random
+
+def cached(size):
+    def decorator(func):
+        cache = {}
+        def wrapper(*args, **kwargs):
+            try:
+                key = (args, frozenset(kwargs.items()))
+            except TypeError:
+                return func(*args, **kwargs)
+
+            if key in cache:
+                return cache[key]
+
+            if len(cache) >= size:
+                ramdom_k = random.choice(list(cache.keys()))
+                del cache[ramdom_k]
+
+            cache[key] = result
+            return result
+
+        return wrapper
+    return decorator
